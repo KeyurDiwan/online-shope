abebb29f6c9b798d82ec305b5f2b1c9ea8bcb545
keyur diwan <keyurdiwan009@gmail.com>
2021-12-31 07:27:51 +0530

    work on pagination..!

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -12,7 +12,11 @@ exports.createProduct = catchAsyncErrors( async (req, res, next) => {
 
 // get all product
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-    const apiFeatures = new ApiFeatures(Product.find(), req.query).search().filter();
+   
+    const resultPerPage = 5;
+    const productCount = await Product.countDocuments();
+
+    const apiFeatures = new ApiFeatures(Product.find(), req.query).search().filter().pagination(resultPerPage);
     const products = await apiFeatures.query;
     res.status(200).json({ success: true, products});
 })
@@ -26,7 +30,7 @@ exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
     }
 
     // await product.remove();
-    res.status(200).json({ success : true, product })
+    res.status(200).json({ success : true, product, productCount })
 })
 
 
--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -35,6 +35,15 @@ class ApiFeatures {
         this.query = this.query.find(JSON.parse(queryStr));
         return this;
     }
+
+    pagination(resultPerPage) {
+        const currentPage = +(this.queryStr.page) || 1;
+        const skip = resultPerPage * (currentPage - 1);
+
+        this.query = this.query.limit(resultPerPage).skip(skip);
+
+        return this;
+    }
 }
 
 module.exports = ApiFeatures;
