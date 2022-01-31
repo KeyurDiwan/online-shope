a6fb6d4d804d218df468d232667717c632ef9903
keyur diwan <keyurdiwan009@gmail.com>
2022-01-31 21:19:11 +0530

    ..

--- frontend/src/component/Home/ProductCard.js
+++ frontend/src/component/Home/ProductCard.js
@@ -5,36 +5,6 @@ import './Home.css'
 
 
 
-// const ProductCard = ( { product } ) => {
-    
-//     const options = {
-//     edit: false,
-//     color: "rgba(20, 20, 20, 0.1)",
-//     activeColor: "tomato",
-//     size: window.innerWidth < 600 ? 20 : 25,
-//     value: product.ratings,
-//     isHalf: true,
-//     }
-    
-//     return (
-//         <Link className="productCard" to= {`product/${product._id}`} >
-//             <img src = {product.images[0].url} alt = "Not found" />
-//             <p>{product.name}</p>
-//             <div>
-//                 <ReactStars {...options} /> {' '}
-//                 <span>
-//                     {' '} ( {product.numOfReviews}  Reviews)
-//                 </span>
-                
-//             </div>
-//             <span> { `Rs. ${product.price}` } </span>
-
-//         </Link>
-//     );
-// };
-
-// export default ProductCard;
-
 const ProductCard = ({ product }) => {
   const options = {
     value: product.ratings,
